0f1abfcdb78b5fb1765e55a6e15def7512c84876
krebso <xkrebs@fi.muni.cz>
2021-03-27 18:15:34 +0100

    Initial commit on branch subdomain

---
+++ app/views/v1/subdomain_util.py
@@ -0,0 +1,5 @@
+# TODO:
+# pamatat si subdomeny v databazi, kazdu chvilu sa bude volat ct search
+# db_models.Target, scanOrder a vlastna tabulka v databazi
+# api_target -> vyclenit co potrebujem aj tu do separatnej metody
+# get_target_id_if_user_can_see_it -> vrati Target z db_models, pouzitie rovnake ako v api_target
