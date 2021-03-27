4aa8af03c2b1eb55dc5b066d3b01ee6afa190eaa
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-03-27 17:56:42 +0100

    Edit scan_scheduler_test to use FreezeGun instead of TimeHelper

--- tests/scan_scheduler_test.py
+++ tests/scan_scheduler_test.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import pytest
 from datetime import datetime, timedelta
-from app.utils.time_helper import time_source
+# from app.utils.time_helper import time_source
 from flask import url_for
 from config import SchedulerConfig
 
@@ -40,52 +40,47 @@ class TestSuiteScanScheduler:
         assert res.status_code == 200
         return res
 
-    def __enable_time_mocking(self):
-        time_source.mock(True)
-        time_source.set_now()
-
-    def test_added_target_with_static_ip_appears_in_scheduler(self):
-        self.__add_target_and_check_presence_in_batch(True)
+    def test_added_target_with_static_ip_appears_in_scheduler(self, freezer):
+        self.__add_target_and_check_presence_in_batch(freezer, True)
 
     @pytest.mark.skip(reason="Dnspython 2.0 has deprecated some functions. Fix later.")
-    def test_added_target_with_dns_appears_in_scheduler(self):
+    def test_added_target_with_dns_appears_in_scheduler(self, freezer):
         # this test needs working DNS for example.com
         # todo: library dnspython updated and deprecated the way our DNS is handled.
-        self.__add_target_and_check_presence_in_batch(False)
+        self.__add_target_and_check_presence_in_batch(freezer, False)
 
-    def __add_target_and_check_presence_in_batch(self, static_ip=True):
-        self.__enable_time_mocking()
+    def __add_target_and_check_presence_in_batch(self, freezer, static_ip=True):
         self.__do_authentication()
 
         ip = "127.0.0.1" if static_ip else None
         self.__add_target(self.__target_add_data(ip=ip))
 
-        time_source.offset_time(timedelta(seconds=SchedulerConfig.max_first_scan_delay) + self.SMALL_OFFSET)
+        freezer.move_to(datetime.now() + timedelta(seconds=SchedulerConfig.max_first_scan_delay) + self.SMALL_OFFSET)
 
         res = self.client.get(url_for("apiV1.api_get_next_targets_batch"))
         assert res.status_code == 200
-        assert len(res.json)>0
+        assert len(res.json) > 0
 
-    def test_requeuing_after_min_separation(self):
-        self.__enable_time_mocking()
+    def test_requeuing_after_min_separation(self, freezer):
         self.__do_authentication()
 
         self.__add_target(self.__target_add_data(ip="127.0.0.1"))
-        time_source.offset_time(timedelta(seconds=SchedulerConfig.max_first_scan_delay) + self.SMALL_OFFSET)
+        freezer.move_to(datetime.now() + timedelta(seconds=SchedulerConfig.max_first_scan_delay) + self.SMALL_OFFSET)
 
         # Scan should be requeued if it did not finish and some time has passed
         for i in range(10):
-            time_source.offset_time(timedelta(seconds=SchedulerConfig.enqueue_min_time) + self.SMALL_OFFSET)
+            freezer.move_to(
+                datetime.now() + timedelta(seconds=SchedulerConfig.enqueue_min_time) + self.SMALL_OFFSET
+            )
             res = self.client.get(url_for("apiV1.api_get_next_targets_batch"))
             assert res.status_code == 200
-            assert len(res.json)>0
+            assert len(res.json) > 0
 
-    def test_not_requeuing_right_away(self):
-        self.__enable_time_mocking()
+    def test_not_requeuing_right_away(self, freezer):
         self.__do_authentication()
 
         self.__add_target(self.__target_add_data(ip="127.0.0.1"))
-        time_source.offset_time(timedelta(seconds=SchedulerConfig.max_first_scan_delay) + self.SMALL_OFFSET)
+        freezer.move_to(datetime.now() + timedelta(seconds=SchedulerConfig.max_first_scan_delay) + self.SMALL_OFFSET)
 
         # First batch should get the target
         res = self.client.get(url_for("apiV1.api_get_next_targets_batch"))
