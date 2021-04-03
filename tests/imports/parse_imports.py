85d1d3bed44f3cfd81e668138e163fa4631eac66
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-04-03 20:13:01 +0200

    Color edges based on meaning

--- tests/imports/parse_imports.py
+++ tests/imports/parse_imports.py
@@ -3,13 +3,18 @@
 from pprint import pprint
 from graphviz import Digraph
 from typing import List
+from enum import Enum
 
 import random
 
-random.seed(42) # used for consistency of colors of edges
 
 SVG_COLORS = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "grey", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"]
 
+class ColorMeaning(Enum):
+    BLACK = 1
+    SOURCE_NODE = 2  # Color based on source node
+    TARGET_NODE = 3
+    
 
 # https://stackoverflow.com/questions/16891340/remove-a-prefix-from-a-string/16891418
 def remove_prefix(text, prefix):
@@ -57,17 +62,32 @@ def process_the_names(lines: List[str]) -> dict:
 
     return b
 
-def create_graph(b: dict, output_filename: str):
+def create_graph(b: dict, output_filename: str, colormeaning: ColorMeaning = ColorMeaning.SOURCE_NODE):
     dot = Digraph()
+    color_map = {}
+
+    random.seed(42) # used for consistency of colors of edges
+
+    for filename in b:
+        color_map[filename] = random.choice(SVG_COLORS)
+    for filename in b:
+        for import_name in b[filename]:
+            color_map[import_name] = color_map.get(import_name, random.choice(SVG_COLORS))
 
     for filename in b:
         dot.node(filename, filename)
 
     for filename in b:
-        color = random.choice(SVG_COLORS)
         for import_name in b[filename]:
-            print(filename, import_name)
-            a = dot.edge(filename, import_name, color=color)
+            # print(filename, import_name)
+            
+            color = "black"
+            if colormeaning == ColorMeaning.SOURCE_NODE:
+                color = color_map[filename]
+            elif colormeaning == ColorMeaning.TARGET_NODE:
+                color = color_map[import_name]
+
+            dot.edge(filename, import_name, color=color)
 
     dot.render(output_filename, view=False)
 
@@ -79,8 +99,10 @@ def main():
         lines = f.readlines()
 
     b = process_the_names(lines)
-    pprint(b)
-    create_graph(b, OUTPUT_FILE)
+    # pprint(b)
+    create_graph(b, f'{OUTPUT_FILE}.black', colormeaning=ColorMeaning.BLACK)
+    create_graph(b, f'{OUTPUT_FILE}.source_file', colormeaning=ColorMeaning.SOURCE_NODE)
+    create_graph(b, f'{OUTPUT_FILE}.imported_file', colormeaning=ColorMeaning.TARGET_NODE)
 
 
 if __name__ == "__main__":
