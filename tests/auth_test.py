de4f5c362248b4774035684e7f2dc6c5f539a18a
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-06-17 14:59:28 +0200

    Rename AuthTestSuiteConfig

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -3,7 +3,7 @@ import pytest
 from flask import url_for
 
 
-class TestSuiteConfig:
+class AuthTestSuiteConfig:
     @staticmethod
     def url_login():
         return url_for("apiV1.api_login")
@@ -33,7 +33,7 @@ class TestSuiteConfig:
 
 def register(func):
     def wrapper(self):
-        assert self.client.post(TestSuiteConfig.url_register(), json=TestSuiteConfig.register_data1).status_code == 200
+        assert self.client.post(AuthTestSuiteConfig.url_register(), json=AuthTestSuiteConfig.register_data1).status_code == 200
         func(self)
     return wrapper
 
@@ -41,8 +41,8 @@ def register(func):
 def login(func):
     def wrapper(self):
         assert self.client.post(
-            TestSuiteConfig.url_login(),
-            json=TestSuiteConfig.login_data_from_register(TestSuiteConfig.register_data1)
+            AuthTestSuiteConfig.url_login(),
+            json=AuthTestSuiteConfig.login_data_from_register(AuthTestSuiteConfig.register_data1)
         ).status_code == 200
         func(self)
     return wrapper
@@ -67,12 +67,12 @@ def set_debug_access_cookie(func):
 class TestSuiteAuth:
 
     def test_login_no_auth(self):
-        assert self.client.post(TestSuiteConfig.url_login()).status_code == 400
+        assert self.client.post(AuthTestSuiteConfig.url_login()).status_code == 400
 
     def test_login_bad_auth(self):
         assert self.client.post(
-            TestSuiteConfig.url_login(),
-            json=TestSuiteConfig.login_data_from_register(TestSuiteConfig.register_data1)
+            AuthTestSuiteConfig.url_login(),
+            json=AuthTestSuiteConfig.login_data_from_register(AuthTestSuiteConfig.register_data1)
         ).status_code == 401
 
     @register
@@ -83,11 +83,11 @@ class TestSuiteAuth:
     @register
     def test_register_two(self):
         # asserts in the decorator itself
-        assert self.client.post(TestSuiteConfig.url_register(), json=TestSuiteConfig.register_data2).status_code == 200
+        assert self.client.post(AuthTestSuiteConfig.url_register(), json=AuthTestSuiteConfig.register_data2).status_code == 200
 
     @register
     def test_register_duplicate(self):
-        assert self.client.post(TestSuiteConfig.url_register(), json=TestSuiteConfig.register_data1).status_code == 400
+        assert self.client.post(AuthTestSuiteConfig.url_register(), json=AuthTestSuiteConfig.register_data1).status_code == 400
 
     @register_and_login
     def test_register_login(self):
@@ -97,8 +97,8 @@ class TestSuiteAuth:
     @register_and_login
     def test_login_with_unregistered_user(self):
         assert self.client.post(
-            TestSuiteConfig.url_login(),
-            json=TestSuiteConfig.login_data_from_register(TestSuiteConfig.register_data2)
+            AuthTestSuiteConfig.url_login(),
+            json=AuthTestSuiteConfig.login_data_from_register(AuthTestSuiteConfig.register_data2)
         ).status_code == 401
 
 
