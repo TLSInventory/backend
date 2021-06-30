e8d7fefe4b12fa828be8bce23c525bc75b10573f
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-06-30 11:36:59 +0200

    Fix JWT test (add route)

--- app/views/debug/tests.py
+++ app/views/debug/tests.py
@@ -1,8 +1,16 @@
-
+import app.utils.authentication_utils
 from . import bp
 
 from app.utils.time_helper import time_source
 
+
 @bp.route('/current_timestamp', methods=['GET'])
 def current_timestamp():
     return str(time_source.timestamp()), 200
+
+
+@bp.route('/test_jwt', methods=['GET'])
+def jwt_inside_route(user_id=None):
+    if user_id is None:
+        user_id = app.utils.authentication_utils.get_user_id_from_current_jwt()
+    return str(user_id), 200
